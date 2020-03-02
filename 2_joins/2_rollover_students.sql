c03a2a4604f7896baae77bbca385995c04a3f51a
Rayesh <rayeshrajah@gmail.com>
2020-03-02 21:51:32 +0000

    join queries went over join in 2_joins directory

--- 2_joins/2_rollover_students.sql
+++ 2_joins/2_rollover_students.sql
@@ -4,6 +4,7 @@ cohorts.name,
 cohorts.start_date AS cohorts_start_date, 
 students.start_date AS student_start_date
 FROM students 
-JOIN cohorts ON cohort_id = cohorts.id
+JOIN cohorts 
+ON cohort_id = cohorts.id
 WHERE cohorts.start_date != students.start_date
-ORDER BY cohorts.name;
+ORDER BY cohorts_name;
