745444345af42c889b016a102362d730f38b8bb9
Rayesh <rayeshrajah@gmail.com>
2020-03-04 23:50:17 +0000

    testing sql injection and learned to use plaecholder and querystring

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,19 +1,25 @@
-const { Pool } = require('pg');
-const pool = new Pool ({
-    user: 'vagrant',
-    password: '123',
-    host: 'localhost',
-    database: 'bootcampx'
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx"
 });
-
-pool.query(`SELECT students.id, students.name, cohorts.name AS cohort 
-            FROM students 
-            JOIN cohorts ON cohorts.id = cohort_id
-            WHERE cohorts.name LIKE '%${process.argv[2]}%'
-            LIMIT ${process.argv[3]};`)
-                     .then(res => {
-                         res.rows.forEach(user => {
-                             console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
-                         })
-                     })
-                     .catch(err => console.error('query error', err.stack));
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+const queryString = `SELECT students.id, students.name, cohorts.name AS cohort 
+                     FROM students 
+                     JOIN cohorts ON cohorts.id = cohort_id
+                     WHERE cohorts.name LIKE $1
+                     LIMIT $2`;
+pool
+  .query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(
+        `${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`
+      );
+    });
+  })
+  .catch(err => console.error("query error", err.stack));
--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -1,21 +1,26 @@
-const { Pool } = require('pg');
-const pool = new Pool ({
-    user: 'vagrant',
-    password: '123',
-    host: 'localhost',
-    database: 'bootcampx'
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx"
 });
 
-pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name AS cohort
-            FROM teachers
-            JOIN assistance_requests ON teachers.id = teacher_id
-            JOIN students ON students.id = student_id 
-            JOIN cohorts ON cohorts.id = cohort_id
-            WHERE cohorts.name LIKE '%${process.argv[2]}%'
-            ORDER BY teacher;`)
-                     .then(res => {
-                         res.rows.forEach(info => {
-                             console.log(`${info.cohort}: ${info.teacher}`);
-                         })
-                     })
-                     .catch(err => console.error('query error', err.stack));
+const cohortName = process.argv[2];
+const values = [cohortName];
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name AS cohort
+                     FROM teachers
+                     JOIN assistance_requests ON teachers.id = teacher_id
+                     JOIN students ON students.id = student_id 
+                     JOIN cohorts ON cohorts.id = cohort_id
+                     WHERE cohorts.name LIKE $1
+                     ORDER BY teacher;`;
+
+pool
+  .query(queryString, values)
+  .then(res => {
+    res.rows.forEach(info => {
+      console.log(`${info.cohort}: ${info.teacher}`);
+    });
+  })
+  .catch(err => console.error("query error", err.stack));
